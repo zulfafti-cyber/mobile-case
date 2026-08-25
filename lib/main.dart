58770c10a05fd6af004d7c902f94c8e1182da538
zulfafti-cyber <zulfafti@fnail.com>
2026-08-25 22:47:30 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+void main() {
+runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+const MyApp({super.key});
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+debugShowCheckedModeBanner: false,
+home: Scaffold(
+appBar: AppBar(
+title: const Text('Pertemuan 1'),
+),
+body: const Center(
+child: Text(
+'Hello World',
+style: TextStyle(fontSize: 24),
+),
+),
+),
+);
+}
+}
